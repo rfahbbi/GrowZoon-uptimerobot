bbf06aca2638fed16da9aa44777cd7487a47c1d3
rfahbbi <79506993+rfahbbi@users.noreply.github.com>
2021-03-02 22:17:19 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://plume-remarkable-sense.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
